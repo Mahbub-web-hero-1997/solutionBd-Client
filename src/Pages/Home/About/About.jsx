e6afbe351569f9b59bbbe5c537fbcb3ad42e923a
rujina-dev-hero <contact.rujina@gmail.com>
2024-02-07 19:13:34 +0600

    About Section Compleat

--- src/Pages/Home/About/About.jsx
+++ src/Pages/Home/About/About.jsx
@@ -5,32 +5,34 @@ const About = () => {
   return (
     <>
       <div className="flex flex-col lg:flex-row h-auto my-5 lg:my-32">
-        <div className="relative">
+        <div className="relative w-full lg:w-1/2">
           <img src={about1} alt="" className="w-full" />
           <img
             src={about2}
             alt=""
-            className="absolute w-1/2 bottom-0 right-0 lg:bottom-[-80px] lg:right-[-80px] opacity-80 lg:opacity-100 "
+            className="absolute w-1/2 lg:w-3/4 bottom-0 right-0 lg:bottom-[-80px] lg:right-[-80px] opacity-80 lg:opacity-100 "
           />
         </div>
-        <div className="lg:ml-32 ">
-          <h4 className="text-xl text-[#FF3811] mt-1 text-center">About Us</h4>
-          <hr className="w-2/6 mb-10 text-[#FF3811] mx-auto" />
-          <h1>We are qualified & of experience in this field</h1>
-          <p>
+        <div className="lg:ml-32 w-full lg:w-1/2 ">
+          <h4 className="text-2xl text-[#FF3811] mt-1 text-center">About Us</h4>
+          <hr className="w-2/6 mb-2 text-[#FF3811] mx-auto" />
+          <h1 className="text-2xl text-center lg:text-left mb-1 font-bold lg:text-3xl">
+            We are qualified & of experience in this field
+          </h1>
+          <p className="text-left mt-3">
             There are many variations of passages of Lorem Ipsum available, but
             the majority have suffered alteration in some form, by injected
             humour, or randomised words which don't look even slightly
             believable.{" "}
           </p>
-          <p>
+          <p className="mt-2">
             the majority have suffered alteration in some form, by injected
             humour, or randomised words which don't look even slightly
             believable.{" "}
           </p>
           <Link
             to="#"
-            className="btn  focus:text-white focus:bg-[#FF3811] me-2"
+            className="btn text-white hover:text-black bg-[#FF3811] me-2 mt-5"
           >
             Get More Info
           </Link>
