72d440e46d18a80a7e47f6a4f64c3fa2a91a93be
Mahbub-web-hero-1997 <mahbub.programing.hero@gmail.com>
2024-02-17 20:26:41 +0600

    Service component added

---
+++ src/Pages/Home/Services/Service.jsx
@@ -0,0 +1,33 @@
+import PropTypes from "prop-types";
+import { FaArrowRight } from "react-icons/fa6";
+import { Link } from "react-router-dom";
+const Service = ({ service }) => {
+  const { title, price, img } = service;
+
+  return (
+    <div className="mx-auto">
+      <div className="card  bg-base-100 shadow-xl h-80">
+        <figure className="">
+          <img src={img} alt="Shoes" className="w-full" />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">
+            {title}
+            <div className="badge badge-secondary">NEW</div>
+          </h2>
+
+          <div className="card-actions justify-between">
+            <b className="text-[#FF3811]">Price: {price}</b>
+            <Link className="text-[#FF3811] focus:text-black" to="#">
+              <FaArrowRight />
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+Service.propTypes = {
+  service: PropTypes.object, // Example type, adjust as needed
+};
+export default Service;
--- src/Pages/Home/Services/Services.jsx
+++ src/Pages/Home/Services/Services.jsx
@@ -1,25 +1,47 @@
 import { useEffect } from "react";
 import { useState } from "react";
+import Service from "./Service";
+import { Link } from "react-router-dom";
+import { FaArrowRight } from "react-icons/fa6";
 
 const Services = () => {
   const [services, setServices] = useState([]);
   useEffect(() => {
     const url = `services.json`;
+    console.log(url);
     fetch(url)
       .then((res) => res.json())
-      .then((data) => console.log(data));
+      .then((data) => setServices(data));
   }, []);
   return (
-    <div>
-      <div className="text-center mb-3">
-        <h3 className=" text-xl text-[#FF3811]">Service</h3>
-        <h1 className="text-3xl mb-2">Our Services</h1>
-        <p className="w-full md:w-1/3 text-center mx-auto">
-          the majority have suffered alteration in some form, by injected
-          humour, or randomised words which dont look even slightly believable.{" "}
-        </p>
+    <>
+      <div className=" w-full lg:w-96">
+        <div className="  text-center mb-3 ">
+          <h3 className=" text-xl text-[#FF3811]">Service</h3>
+          <h1 className="text-3xl mb-2">Our Services</h1>
+          <p className=" w-full md:w-1/3 text-center mx-auto">
+            the majority have suffered alteration in some form, by injected
+            humour, or randomised words which dont look even slightly
+            believable.{" "}
+          </p>
+        </div>
       </div>
-    </div>
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-y-6  ">
+        {services.map((service) => (
+          <Service key={service._id} service={service} />
+        ))}
+      </div>
+
+      <Link
+        to="#"
+        className=" flex items-center justify-center text-[#FF3811] font-bold cursor-pointer gap-1 my-8 "
+      >
+        More Service
+        <span className="mt-1">
+          <FaArrowRight />
+        </span>
+      </Link>
+    </>
   );
 };
 
