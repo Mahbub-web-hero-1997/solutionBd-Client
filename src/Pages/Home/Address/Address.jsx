b03916b5bf0eb13e99a62247de2faff2b643910c
Mahbub-web-hero-1997 <mahbub.programing.hero@gmail.com>
2024-02-20 15:48:04 +0600

    Contact and address added

--- src/Pages/Home/Address/Address.jsx
+++ src/Pages/Home/Address/Address.jsx
@@ -1,13 +1,34 @@
 import { FaClock } from "react-icons/fa";
+import { FaPhoneSquareAlt } from "react-icons/fa";
+import { FaLocationDot } from "react-icons/fa6";
 const Address = () => {
   return (
-    <div className="w-full  bg-black">
-      <div className=" bg-red-500 flex justify-between text-white text-center">
-        <div className="w-full ">
+    <div className=" grid grid-cols-1 mb-5 lg:mb-0 gap-x-4 md:grid-cols-3 w-full bg-black h-auto lg:h-52 p-5 rounded-lg">
+      <div className=" flex items-center lg:justify-center text-white h-28 lg:h-auto">
+        <span className="text-3xl text-[#FF3811]">
           <FaClock />
-          <div>
-            <p>We are open monday-friday</p>
-          </div>
+        </span>
+        <div className="ml-3 text-center">
+          <p>We are open monday-friday</p>
+          <b className="text-xl">7:00 am - 9:00 pm</b>
+        </div>
+      </div>
+      <div className=" flex mb-5 lg:mb-0  items-center lg:justify-center text-white h-28 lg:h-auto">
+        <span className="text-3xl text-[#FF3811]">
+          <FaPhoneSquareAlt />
+        </span>
+        <div className="ml-3 text-center">
+          <p>Have a question?</p>
+          <b className="text-xl">+2546 251 2658</b>
+        </div>
+      </div>
+      <div className=" flex mb-5 lg:mb-0  items-center lg:justify-center text-white h-28 lg:h-auto">
+        <span className="text-3xl text-[#FF3811]">
+          <FaLocationDot />
+        </span>
+        <div className="ml-3 text-center">
+          <p>Need a repair? our address</p>
+          <b className="text-xl">Liza Street, New York</b>
         </div>
       </div>
     </div>
