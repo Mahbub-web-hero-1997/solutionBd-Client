5b8562939f9c1201038a62aeb17f45c5fb72af2b
Mahbub-web-hero-1997 <mahbub.programing.hero@gmail.com>
2024-07-27 15:04:59 +0600

    some changes

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -10,20 +10,22 @@ const Login = () => {
     
     const handleLogin = (event) => {
         const form = event.target;
-        const name = form.first.value;
-        console.log(name)
-        event.preventDefault();    
-    
+        const email = form.email.value;
+        const password = form.password.value;
+        const user={email:email, password:password}
+        console.log(user)
+        event.preventDefault();      
         
     }
     return (
         <>
            
-            <div className=' Login_Form_Container container lg:w-1/2  my-10 mx-auto p-5'>
-                <h1 className='text-2xl font-semibold text-[#FF3811]'>Please Login! </h1>
-                 <form action="" className='flex flex-col items-center justify-center gap-5'>
-                    <input className='w-full p-3 outline-none text-lg border-[#FF3811] border-b' type="text" name="email" id="" placeholder='Enter Your Email'  />
-                    <input className='w-full p-3 outline-none text-lg border-[#FF3811] border-b' type="password" name="password" id="" placeholder='Enter Your Password' />
+            <div className=' Login_Form_Container container lg:w-1/2  pb-10 mb-5 mx-auto p-5'>
+                <h1 className='text-2xl font-semibold text-[#FF3811] mb-4'>Please Login.</h1>
+                
+                 <form onSubmit={handleLogin} action="" className='flex flex-col items-center justify-center gap-5'>
+                    <input className='w-full p-3 pl-0 outline-none text-lg border-[#FF3811] border-b' type="text" name="email" id="" placeholder='Enter Your Email'  />
+                    <input className='w-full p-3 pl-0 outline-none text-lg border-[#FF3811] border-b' type="password" name="password" id="" placeholder='Enter Your Password' />
                     <input type="submit" value="Login" className='w-full bg-[#FF3811] p-3 text-white font-semibold text-xl cursor-pointer rounded-sm' />
                 </form>
                 <div className='flex w-full mt-3 '>
