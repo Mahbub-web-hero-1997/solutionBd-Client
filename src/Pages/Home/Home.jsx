77180bdb25f495b72ab9b8ffd23c10460dc07eda
Mahbub-web-hero-1997 <mahbub.programing.hero@gmail.com>
2024-09-07 09:40:53 +0600

    some changes

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,18 +1,15 @@
 import Banner from "../Banner/Banner";
-import About from "./About/About";
+
 import Address from "./Address/Address";
 import Services from "./Services/Services";
 // import Service from "./Services/Service";
-
 
 const Home = () => {
   return (
     <div>
       <Banner />
-      <About />
       <Services />
-      {/* <Service/> */}
-      <Address />    
+      <Address />
     </div>
   );
 };
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -3,61 +3,45 @@ import Root from "../Pages/Root";
 import Home from "../Pages/Home/Home";
 import ServiceDetails from "../Pages/ServiceDetails/ServiceDetails";
 import Services from "../Pages/Home/Services/Services";
-import About from "../Pages/Home/About/About";
-import CheckOut from "../Pages/CheckOut/CheckOut";
 import Login from "../Pages/Login/Login";
 import Register from "../Pages/Register/Register";
 import Bookings from "../Pages/Booking/Bookings";
-// import Banner from "../Pages/Banner/Banner";
 
+// import Banner from "../Pages/Banner/Banner";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
     children: [
-    
       {
         path: "/",
         element: <Home />,
       },
       {
         path: "/services",
-        element: <Services/>, 
-              
-        },
+        element: <Services />,
+        loader: () => fetch("http://localhost:5000/services"),
+      },
       {
         path: "services/:id",
         element: <ServiceDetails />,
-        loader:({params})=>fetch(`http://localhost:5000/services/${params.id}`)        
-        
-      },     
-      {
-        path: "checkout/:id",
-        element: <CheckOut />,
-        loader:({params})=>fetch(`http://localhost:5000/services/${params.id}`)      
-        
-      },
-      {
-        path: "/about",
-        element:<About/>
+        loader: ({ params }) =>
+          fetch(`http://localhost:5000/services/${params.id}`),
       },
       {
         path: "/login",
-        element:<Login/>
+        element: <Login />,
       },
       {
         path: "/register",
-        element:<Register/>
+        element: <Register />,
       },
       {
         path: "/bookings",
         element: <Bookings />,
-        loader: () => fetch('http://localhost:5000/booking')
-      }
-      
-     
+        loader: () => fetch("http://localhost:5000/booking"),
+      },
     ],
-  },  
-
+  },
 ]);
